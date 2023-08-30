3545042eb702f6a1732d39eb5281b27cecf9686e
Smitty81-webs <82186230+Smitty81-webs@users.noreply.github.com>
2023-08-30 18:29:58 -0500

    Nisswa Smiles dental office

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
