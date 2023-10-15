77ba7023acdf8413cd4f424a96f6e28ebc919b01
Smitty81-webs <82186230+Smitty81-webs@users.noreply.github.com>
2023-10-15 17:31:45 -0500

    added bounce to facebook icon, contact page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
-      
+      animation: {
+        // Bounces 5 times 1s equals 5 seconds
+        'bounce': 'bounce 1s ease-in-out 10',
+      },
     },
   },
   plugins: [
